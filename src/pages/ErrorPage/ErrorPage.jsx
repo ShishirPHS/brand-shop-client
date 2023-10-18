2e8c4328695f7ef1a64807f967df6839fb06fba2
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-19 03:55:03 +0600

    update error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h2>This is error page</h2>
+    <div className="container mx-auto">
+      <div className="flex flex-col items-center justify-center h-[70vh] space-y-5">
+        <h2 className="font-bold text-3xl">Page Not Found</h2>
+        <NavLink to="/">
+          <button className="btn">Go to home</button>
+        </NavLink>
+      </div>
     </div>
   );
 };
