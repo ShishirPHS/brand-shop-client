a6b4cf50232c7a5bafd2eb7e88151ae38a3c62ca
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-20 04:46:19 +0600

    added slider to brand based dynamic route

--- src/pages/BrandedProduct/BrandedProduct.jsx
+++ src/pages/BrandedProduct/BrandedProduct.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import Header from "../../components/Header/Header";
 
 const BrandedProduct = () => {
   const brandedProducts = useLoaderData();
@@ -20,12 +21,52 @@ const BrandedProduct = () => {
     (singleAdImg) => singleAdImg.brand_name == brandName
   );
 
-  console.log(adImgForSelectedBrand);
-
   return (
-    <div className="container mx-auto">
-      <h2>brand based product will show here</h2>
-      <div>{selectedBrandedProducts.length}</div>
+    <div>
+      <Header></Header>
+      <div>
+        <div className="container mx-auto">
+          <div>
+            <div className="carousel w-full">
+              <div id="slide1" className="carousel-item relative w-full">
+                <img src={adImgForSelectedBrand[0]?.image} className="" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide3" className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href="#slide2" className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+              </div>
+              <div id="slide2" className="carousel-item relative w-full">
+                <img src={adImgForSelectedBrand[1]?.image} className="" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide1" className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href="#slide3" className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+              </div>
+              <div id="slide3" className="carousel-item relative w-full">
+                <img src={adImgForSelectedBrand[2]?.image} className="" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide2" className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href="#slide1" className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h2>brand based product will show here</h2>
+          <div>{selectedBrandedProducts.length}</div>
+        </div>
+      </div>
     </div>
   );
 };
