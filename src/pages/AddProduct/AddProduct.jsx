26dd8260421e4ee406dc78af5790a3ed5a7ac13b
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-20 20:26:50 +0600

    added navbar to addProduct and updateProduct

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,4 +1,5 @@
 import Swal from "sweetalert2";
+import Header from "../../components/Header/Header";
 
 const AddProduct = () => {
   const handleAddProduct = (e) => {
@@ -38,122 +39,127 @@ const AddProduct = () => {
   };
 
   return (
-    <div className="container mx-auto mt-24">
-      <h2 className="text-center text-4xl font-bold mb-20">Add Product</h2>
-      <form onSubmit={handleAddProduct} className="w-3/4 mx-auto mb-32">
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Image</span>
-          </label>
-          <input
-            type="text"
-            name="image"
-            placeholder="Image URL"
-            className="w-full  input input-bordered"
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Name</span>
-          </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Product Name"
-            className="w-full  input input-bordered"
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Brand Name</span>
-          </label>
-          {/* <input
+    <div>
+      <Header></Header>
+      <div className="container mx-auto mt-24">
+        <h2 className="text-center text-4xl font-bold mb-20">Add Product</h2>
+        <form onSubmit={handleAddProduct} className="w-3/4 mx-auto mb-32">
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Image</span>
+            </label>
+            <input
+              type="text"
+              name="image"
+              placeholder="Image URL"
+              className="w-full  input input-bordered"
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Product Name"
+              className="w-full  input input-bordered"
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Brand Name</span>
+            </label>
+            {/* <input
             type="text"
             name="brand"
             placeholder="Brand Name"
             className="w-full  input input-bordered"
             required
           /> */}
-          <select
-            className="w-full border-2 rounded-lg py-3 px-2"
-            name="brand"
-            id=""
-            required
-          >
-            <option value="Nike">Nike</option>
-            <option value="Adidas">Adidas</option>
-            <option value="Gucci">Gucci</option>
-            <option value="Zara">Zara</option>
-            <option value="H&M">H&M</option>
-            <option value="Levi's">Levis</option>
-          </select>
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Type</span>
-          </label>
-          <input
-            type="text"
-            name="type"
-            placeholder="Product Type"
-            className="w-full  input input-bordered"
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Price</span>
-          </label>
-          <input
-            type="text"
-            name="price"
-            placeholder="Price"
-            className="w-full  input input-bordered"
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Short Description</span>
-          </label>
-          <input
-            type="text"
-            name="description"
-            placeholder="Short description of product"
-            className="w-full  input input-bordered"
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Rating</span>
-          </label>
-          {/* <input
+            <select
+              className="w-full border-2 rounded-lg py-3 px-2"
+              name="brand"
+              id=""
+              required
+            >
+              <option value="Nike">Nike</option>
+              <option value="Adidas">Adidas</option>
+              <option value="Gucci">Gucci</option>
+              <option value="Zara">Zara</option>
+              <option value="H&M">H&M</option>
+              <option value="Levi's">Levis</option>
+            </select>
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Type</span>
+            </label>
+            <input
+              type="text"
+              name="type"
+              placeholder="Product Type"
+              className="w-full  input input-bordered"
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Price</span>
+            </label>
+            <input
+              type="text"
+              name="price"
+              placeholder="Price"
+              className="w-full  input input-bordered"
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">
+                Short Description
+              </span>
+            </label>
+            <input
+              type="text"
+              name="description"
+              placeholder="Short description of product"
+              className="w-full  input input-bordered"
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Rating</span>
+            </label>
+            {/* <input
             type="text"
             name="rating"
             placeholder="Rating out of 5"
             className="w-full  input input-bordered"
             required
           /> */}
-          <select
-            className="w-full border-2 rounded-lg py-3 px-2"
-            name="rating"
-            id=""
-            required
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </div>
-        <div className="mt-6">
-          <input className="btn" type="submit" value="Add Product" />
-        </div>
-      </form>
+            <select
+              className="w-full border-2 rounded-lg py-3 px-2"
+              name="rating"
+              id=""
+              required
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div className="mt-6">
+            <input className="btn" type="submit" value="Add Product" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/UpdateProduct/UpdateProduct.jsx
+++ src/pages/UpdateProduct/UpdateProduct.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import Swal from "sweetalert2";
+import Header from "../../components/Header/Header";
 
 const UpdateProduct = () => {
   const product = useLoaderData();
@@ -39,116 +40,119 @@ const UpdateProduct = () => {
   };
 
   return (
-    <div className="container mx-auto mt-24">
-      <h2 className="text-center text-4xl font-bold mb-20">Update Product</h2>
-      <form onSubmit={handleUpdateProduct} className="w-3/4 mx-auto mb-32">
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Image</span>
-          </label>
-          <input
-            type="text"
-            name="image"
-            placeholder="Image URL"
-            className="w-full  input input-bordered"
-            defaultValue={image}
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Name</span>
-          </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Product Name"
-            className="w-full  input input-bordered"
-            defaultValue={name}
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Brand Name</span>
-          </label>
-          {/* <input
+    <div>
+      <Header></Header>
+      <div className="container mx-auto mt-24">
+        <h2 className="text-center text-4xl font-bold mb-20">Update Product</h2>
+        <form onSubmit={handleUpdateProduct} className="w-3/4 mx-auto mb-32">
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Image</span>
+            </label>
+            <input
+              type="text"
+              name="image"
+              placeholder="Image URL"
+              className="w-full  input input-bordered"
+              defaultValue={image}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Product Name"
+              className="w-full  input input-bordered"
+              defaultValue={name}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Brand Name</span>
+            </label>
+            {/* <input
             type="text"
             name="brand"
             placeholder="Brand Name"
             className="w-full  input input-bordered"
             required
           /> */}
-          <select
-            className="w-full border-2 rounded-lg py-3 px-2"
-            name="brand"
-            id=""
-            defaultValue={brand}
-            required
-          >
-            <option value="Nike">Nike</option>
-            <option value="Adidas">Adidas</option>
-            <option value="Gucci">Gucci</option>
-            <option value="Zara">Zara</option>
-            <option value="H&M">H&M</option>
-            <option value="Levi's">Levis</option>
-          </select>
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Type</span>
-          </label>
-          <input
-            type="text"
-            name="type"
-            placeholder="Product Type"
-            className="w-full  input input-bordered"
-            defaultValue={type}
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Price</span>
-          </label>
-          <input
-            type="text"
-            name="price"
-            placeholder="Price"
-            className="w-full  input input-bordered"
-            defaultValue={price}
-            required
-          />
-        </div>
-        <div className="">
-          <label className="label">
-            <span className="label-text font-semibold">Rating</span>
-          </label>
-          {/* <input
+            <select
+              className="w-full border-2 rounded-lg py-3 px-2"
+              name="brand"
+              id=""
+              defaultValue={brand}
+              required
+            >
+              <option value="Nike">Nike</option>
+              <option value="Adidas">Adidas</option>
+              <option value="Gucci">Gucci</option>
+              <option value="Zara">Zara</option>
+              <option value="H&M">H&M</option>
+              <option value="Levi's">Levis</option>
+            </select>
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Type</span>
+            </label>
+            <input
+              type="text"
+              name="type"
+              placeholder="Product Type"
+              className="w-full  input input-bordered"
+              defaultValue={type}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Price</span>
+            </label>
+            <input
+              type="text"
+              name="price"
+              placeholder="Price"
+              className="w-full  input input-bordered"
+              defaultValue={price}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="label">
+              <span className="label-text font-semibold">Rating</span>
+            </label>
+            {/* <input
             type="text"
             name="rating"
             placeholder="Rating out of 5"
             className="w-full  input input-bordered"
             required
           /> */}
-          <select
-            className="w-full border-2 rounded-lg py-3 px-2"
-            name="rating"
-            id=""
-            defaultValue={rating}
-            required
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </div>
-        <div className="mt-6">
-          <input className="btn" type="submit" value="Update Product" />
-        </div>
-      </form>
+            <select
+              className="w-full border-2 rounded-lg py-3 px-2"
+              name="rating"
+              id=""
+              defaultValue={rating}
+              required
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div className="mt-6">
+            <input className="btn" type="submit" value="Update Product" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
